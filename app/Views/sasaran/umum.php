a44ceb064dd79b807995e117812f418db6e47257
danuri <danualbantani@gmail.com>
2024-08-01 17:15:27 +0700

    asset

--- app/Views/sasaran/umum.php
+++ app/Views/sasaran/umum.php
@@ -1,9 +1,9 @@
 <?= $this->extend('template') ?>
 
 <?= $this->section('style') ?>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="<?= base_url()?>assets/libs/datatables/dataTables.bootstrap5.min.css" />
+<link rel="stylesheet" href="<?= base_url()?>assets/libs/datatables/responsive.bootstrap.min.css" />
+<link rel="stylesheet" href="<?= base_url()?>assets/libs/datatables/buttons.dataTables.min.css">
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
@@ -126,10 +126,10 @@
 <?= $this->endSection() ?>
 <?= $this->section('script') ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="<?= base_url()?>assets/libs/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url()?>assets/libs/datatables/dataTables.bootstrap5.min.js"></script>
+<script src="<?= base_url()?>assets/libs/datatables/dataTables.responsive.min.js"></script>
+<script src="<?= base_url()?>assets/libs/select2/select2.min.js"></script>
 <script>
 $(document).ready(function() {
 
