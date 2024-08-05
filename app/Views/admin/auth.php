817f31bbde492c6627f9eb3cb5ecb1fa6276ee47
danuri <danualbantani@gmail.com>
2024-08-05 14:21:24 +0700

    Update auth.php

--- app/Views/admin/auth.php
+++ app/Views/admin/auth.php
@@ -2,9 +2,9 @@
 <html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
 <head>
     <meta charset="utf-8" />
-    <title>Admin Layanan Kepegawai | Kementerian Agama RI</title>
+    <title>Admin Sistem Penyuluh Agama | Kementerian Agama RI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Layanan Kepegawaian Biro Kepegawaian Kementerian Agama RI" name="description" />
+    <meta content="Penyuluh Agama Kementerian Agama RI" name="description" />
     <meta content="Danunih" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="<?= base_url()?>assets/images/favicon.ico">
