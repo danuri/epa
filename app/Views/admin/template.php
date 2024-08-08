fe932f4a9828dfd32e51c867b0968ccdd1599cb5
danuri <danualbantani@gmail.com>
2024-08-08 17:24:40 +0700

    Update template.php

--- app/Views/admin/template.php
+++ app/Views/admin/template.php
@@ -267,6 +267,30 @@
     <script src="<?= base_url()?>assets/js/app.js"></script>
     <script type="text/javascript">
       var site_url = '<?= site_url('admin')?>';
+
+      function alert($text) {
+        Toastify({
+          text: $text,
+          duration: 5000,
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "center", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function(){} // Callback after click
+        }).showToast();
+      }
+
+      <?php
+      if(session()->getFlashdata('message')){
+        ?>
+        alert("<?= session()->getFlashdata('message')?>");
+        <?php
+      }
+      ?>
     </script>
     <?= $this->renderSection('script') ?>
 </body>
