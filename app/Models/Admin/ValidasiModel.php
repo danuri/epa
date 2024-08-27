1b5d5e3389a5dbdaa419629a1c46d738ddb59309
danuri <danualbantani@gmail.com>
2024-08-27 09:48:06 +0700

    Update ValidasiModel.php

--- app/Models/Admin/ValidasiModel.php
+++ app/Models/Admin/ValidasiModel.php
@@ -12,7 +12,7 @@ class ValidasiModel extends Model
     protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['status_pegawai_validasi','nip','nik','kode_satker','nama_satker'];
+    protected $allowedFields    = ['status_pegawai_validasi','nip','nik','kode_satker','nama_satker','keterangan'];
 
     protected bool $allowEmptyInserts = false;
 
